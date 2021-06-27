d05ee991085584faeba871ed928fb94f8cb0e9f9
virysik <68701496+virysik@users.noreply.github.com>
2021-06-27 13:57:13 +0200

    created Statistics file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import Profile from './Profile';
+import Statistics from './Statistics';
 import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
 
 function App() {
   return (
-    <Profile
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
-    />
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+    </>
   );
 }
 
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import s from './Profile.module.css';
 
-function Profile({ name, tag, location, avatar, followers, views, likes }) {
+function Profile({ name, tag, location, avatar, stats }) {
   return (
     <div className={s.profile}>
       <div className={s.description}>
@@ -14,15 +14,15 @@ function Profile({ name, tag, location, avatar, followers, views, likes }) {
       <ul className={s.stats}>
         <li className={s.li}>
           <span className={s.label}>Followers</span>
-          <span className={s.quantity}>{followers}</span>
+          <span className={s.quantity}>{stats.followers}</span>
         </li>
         <li className={s.li}>
           <span className={s.label}>Views</span>
-          <span className={s.quantity}>{views}</span>
+          <span className={s.quantity}>{stats.views}</span>
         </li>
         <li className={s.li}>
           <span className={s.label}>Likes</span>
-          <span className={s.quantity}>{likes}</span>
+          <span className={s.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
@@ -36,11 +36,9 @@ Profile.defaultProps = {
 };
 
 Profile.propTypes = {
-  name: PropTypes.string,
-  tag: PropTypes.string,
-  location: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
   avatar: PropTypes.string,
-  followers: PropTypes.number,
-  views: PropTypes.number,
-  likes: PropTypes.number,
+  stats: PropTypes.objectOf(PropTypes.number),
 };
