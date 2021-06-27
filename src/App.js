02335f6abf4b689f0407099c64e772752fc89a29
virysik <68701496+virysik@users.noreply.github.com>
2021-06-27 11:40:42 +0200

    added back icons

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import user from './data/user.json';
 
 function App() {
   return (
-    <div className="App">
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </div>
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 }
 
