4c5d025587bc303104a0dfee6b1dcabbb613481d
virysik <68701496+virysik@users.noreply.github.com>
2021-06-27 11:33:20 +0200

    added Profile component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Profile from './Profile';
+import user from './data/user.json';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
+
+export default App;
